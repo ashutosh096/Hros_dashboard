ed617f8c02f464474a791ffb4801c46706b20bb3
Ashutosh Mishra <ashutoshmishraup78@gmail.com>
2026-07-17 16:42:55 +0530

    docs: improve JSDoc comments in table.tsx

--- artifacts/hr-dashboard/src/components/ui/table.tsx
+++ artifacts/hr-dashboard/src/components/ui/table.tsx
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview Custom UI Grid table templates.
+ * Structures data grid cells, headers, and footers.
+ */
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
