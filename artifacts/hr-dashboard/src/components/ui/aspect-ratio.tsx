e0d7b7bc95fa32ef0c95984b21c853d118fd665d
Ashutosh Mishra <ashutoshmishraup78@gmail.com>
2026-07-17 16:35:38 +0530

    docs: improve JSDoc comments in aspect-ratio.tsx

--- artifacts/hr-dashboard/src/components/ui/aspect-ratio.tsx
+++ artifacts/hr-dashboard/src/components/ui/aspect-ratio.tsx
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview Custom UI Aspect Ratio wrapper component.
+ * Restricts height proportionally relative to width.
+ */
+
 import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
 
 const AspectRatio = AspectRatioPrimitive.Root
